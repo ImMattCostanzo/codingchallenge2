31a2e1dbbd7ad32e32db3c836a948ed06fa68c73
Matthew Costanzo <Matt@c02lp093fd57.home>
2022-04-15 13:55:26 -0400

    Confusion

---
+++ javascript.js
@@ -0,0 +1,6 @@
+let money = prompt("Deposit Money Or Withdraw Money?");
+console.log(money);
+
+var deposit= "Deposit Money"
+var withdraw = "Withdraw Money"
+if (deposit = "Deposit Money") {console.log(“How much would you like to deposit?");}
